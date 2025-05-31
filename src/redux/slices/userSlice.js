591d9b51de12c4d94985985670739f8f43e924ad
Priyanka Prajapati <priyanka.prajapati@nuvolo.com>
2025-05-31 14:41:27 +0530

    feat: create user slice

---
+++ src/redux/slices/userSlice.js
@@ -0,0 +1,12 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+
+
+export const userSlice = createSlice({
+    name: "user",
+    initialState : null,
+    reducers:{}
+})
+
+
+export default userSlice.reducer;
