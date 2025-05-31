f9e313694167c4e636ba4f618919bf479099d737
Priyanka Prajapati <priyanka.prajapati@nuvolo.com>
2025-05-31 15:39:13 +0530

    feat: Added navigation to feed page

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import { BrowserRouter, Routes, Route } from "react-router";
 import "./App.css";
 import { Home, Login, Profile } from "./components";
+import { Feed } from "./components/Feed";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />}>
+        <Route path="/" element={<Feed />} />
           <Route path="/login" element={<Login />} />
           <Route path="/profile" element={<Profile />} />
+         
         </Route>
       </Routes>
     </BrowserRouter>
--- src/redux/slices/userSlice.js
+++ src/redux/slices/userSlice.js
@@ -1,12 +1,15 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-
-
 export const userSlice = createSlice({
-    name: "user",
-    initialState : null,
-    reducers:{}
-})
+  name: "user",
+  initialState: null,
+  reducers: {
+    addUser: (state, action) => {
+      return action.payload;
+    },
+  },
+});
 
+export const { addUser } = userSlice.actions;
 
 export default userSlice.reducer;
