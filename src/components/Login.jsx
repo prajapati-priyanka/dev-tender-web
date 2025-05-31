9f5644b9f0fa0ceca8300bc8720ea692b42f8b45
Priyanka Prajapati <priyanka.prajapati@nuvolo.com>
2025-05-31 14:37:19 +0530

    feat: add login page and functionality

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,6 +1,56 @@
-export const Login = ()=>{
-    return(
-        <>Login Page</>
-    )
-}
+import { useState } from "react";
+import axios from "axios";
 
+export const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = async () => {
+    try {
+      const response = await axios.post("http://localhost:3000/login", {
+        email: email,
+        password: password,
+      },{withCredentials: true });
+      console.log("response", response);
+    } catch (error) {
+      console.error("Error Occurred: ", error.message);
+    }
+  };
+
+  return (
+    <div className="flex justify-center mt-28">
+      <div className="card card-border bg-base-100 w-96 border-inherit">
+        <div className="card-body">
+          <h2 className="card-title underline justify-center">Login</h2>
+          <div>
+            <fieldset className="fieldset">
+              <legend className="fieldset-legend">Email</legend>
+              <input
+                type="text"
+                value={email}
+                className="input"
+                placeholder="Type here"
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </fieldset>
+            <fieldset className="fieldset">
+              <legend className="fieldset-legend">Password</legend>
+              <input
+                type="text"
+                value={password}
+                className="input"
+                placeholder="Type here"
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </fieldset>
+          </div>
+          <div className="card-actions justify-center">
+            <button className="btn btn-primary" onClick={handleLogin}>
+              Login
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
