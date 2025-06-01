752b5e93eb6e4f69b8d350f418028a19e467188a
Priyanka Prajapati <priyanka.prajapati@nuvolo.com>
2025-06-01 16:14:17 +0530

    feat: Add Edit Profile Page

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,5 +1,8 @@
-export const Profile = ()=>{
-    return(
-        <h1>Profile Page</h1>
-    )
-}
+import { useSelector } from "react-redux";
+import EditProfile from "./EditProfile";
+
+export const Profile = () => {
+  const user = useSelector((state) => state.user);
+
+  return <>{user && <EditProfile user={user} />}</>;
+};
--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -1,18 +1,21 @@
-export const UserCard = ({feed}) => {
-    const {firstName, lastName, photoUrl,about} = feed;
+export const UserCard = (props) => {
+  const { firstName, lastName, photoUrl, about, gender, age } = props.user;
   return (
-    <div className="card bg-base-200 w-96 shadow-sm m-auto my-15">
+    <div className="card bg-base-200 w-96 shadow-sm my-15">
       <figure>
-        <img
-          src={photoUrl}
-          alt="photos"
-        />
+        <img src={photoUrl} alt="photos" />
       </figure>
       <div className="card-body">
-        <h2 className="card-title">{firstName} {lastName}</h2>
-        <p>
-        {about}
-        </p>
+        <h2 className="card-title">
+          {firstName} {lastName}
+        </h2>
+        <div className="flex gap-2">
+        {age && <span>{age}</span>}
+        {gender && <span>{gender}</span>}
+        </div>
+        
+
+        <p>{about}</p>
         <div className="card-actions justify-center my-5">
           <button className="btn btn-secondary">Ignored</button>
           <button className="btn btn-primary">Interested</button>
